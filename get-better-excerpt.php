9779c271ed6a97c64d890f60ba9c37b93f8da1da
christopherross <christopherross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-07 14:00:22 +0000

    git-svn-id: https://plugins.svn.wordpress.org/get-better-excerpt/trunk@123543 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ get-better-excerpt.php
@@ -0,0 +1,164 @@
+<?php
+/*
+Plugin Name: Get Better Excerpt 
+Plugin URI: http://thisismyurl.com/plugins/get-better-excerpt
+Description: An easy to use WordPress function to add scheduled posts to any theme.
+Author: Christopher Ross
+Tags: future, upcoming posts, upcoming post, upcoming, draft, Post, scheduled, preview
+Author URI: http://thisismyurl.com
+Version: 0.1.1
+*/
+
+/*  Copyright 2008  Christopher Ross  (email : info@thisismyurl.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+
+add_action('admin_menu', 'Easyget_better_excerpt_menu');
+
+function Easyget_better_excerpt_menu() {
+  add_options_page('Get Better Excerpt', 'Get Better Excerpt', 10,'Easyget_better_excerpt.php', 'Easyget_better_excerpt_options');
+}
+
+function Easyget_better_excerpt_options() {
+
+?>
+<div class="wrap">
+    <div id="icon-options-general" class="icon32"><br /></div>
+    <h2>Get Better Excerpt Settings</h2>
+    
+    
+    
+    <div id="poststuff" class="metabox-holder">
+    <div class="inner-sidebar">
+    <div id="side-sortables" class="meta-box-sortabless ui-sortable" style="position:relative;">
+    
+    <div id="sm_pnres" class="postbox">
+    <h3 class="hndle"><span>About this Plugin:</span></h3>
+    <div class="inside">
+    <ul class='options'>
+    <style>.options a {text-decoration:none;}</style>
+    <li><a href="http://www.thisismyurl.com/free-downloads/get-better-excerpt/">Plugin Homepage</a></li>
+    <li><a href="http://wordpress.org/extend/plugins/get-better-excerpt/">Vote for this Plugin</a></li>
+    <li><a href="http://forums.thisismyurl.com/">Support Forum</a></li>
+    <li><a href="http://support.thisismyurl.com/">Report a Bug</a></li>
+    <li><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5962435">Donate with PayPal</a></li>
+    </ul>
+    </div>
+    </div>
+    
+    </div>
+    </div>
+    
+    <div class="has-sidebar sm-padded" >
+    
+    <div id="post-body-content" class="has-sidebar-content">
+    
+    <div class="meta-box-sortabless">
+    
+    <!-- Rebuild Area -->
+    <!-- Basic Options -->
+    <div id="sm_basic_options" class="postbox">
+    <h3 class="hndle"><span>Basic Options</span></h3>
+    <div class="inside">
+    <p class="hndle">This plugin has no Administation level settings. </p>
+    </div>
+    </div>
+    
+    <div id="sm_basic_options2" class="postbox">
+      <h3 class="hndle"><span>Read Me File Contents</span></h3>
+    <div class="inside">
+      <?php 
+	  $contents = file_get_contents('../wp-content/plugins/get-better-excerpt/readme.txt');
+	  $contents = str_replace("\n","<br>",$contents);
+	  echo $contents;
+	  ?>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+</div>
+<?php
+}
+
+
+
+
+
+
+
+
+
+
+function get_better_excerpt($options='') {
+    global $post, $wpdb;
+	
+	$ns_options = array(
+                    "words" => "20",
+                    "before"  => "",
+                    "after" => "",
+					"show" => false,
+					"skipexcerpt" => true,
+					"link" => false,
+					"trail" => "&#8230;",
+                   );
+
+	$options = explode("&",$options);
+	
+	foreach ($options as $option) {
+	
+		$parts = explode("=",$option);
+		$options[$parts[0]] = $parts[1];
+	
+	}
+	
+	if ($options['words']) {$ns_options['words'] = $options['words'];}
+	if ($options['before']) {$ns_options['before'] = $options['before'];}
+	if ($options['after']) {$ns_options['after'] = $options['after'];}
+	if ($options['show']) {$ns_options['show'] = $options['show'];}
+	if ($options['skipexcerpt']) {$ns_options['skipexcerpt'] = $options['skipexcerpt'];}
+	if ($options['link']) {$ns_options['link'] = $options['link'];}
+	if ($options['trail']) {$ns_options['trail'] = $options['trail'];}
+	
+	global $more;
+	$more = 1;
+	
+	if ($ns_options['skipexcerpt']) {
+		$excerpt = explode(' ', strip_tags(get_the_content()), $ns_options['words']+1);
+	} else {
+		$excerpt = explode(' ', strip_tags(get_the_excerpt()), $ns_options['words']+1);
+	}
+	array_pop($excerpt);
+	$excerpt = implode(" ",$excerpt);
+	$excerpt = $excerpt.$ns_options['trail'];
+	
+	if ($link) {
+		$link = get_permalink();
+		$title = get_the_title();
+		$excerpt = "<a href='$link' title='$title'>".$excerpt."</a>";
+	}
+	
+	$excerpt = $ns_options['before'].$excerpt.$ns_options['after'];
+
+	if ($show) {echo $excerpt;} else {return $excerpt;}
+	
+}
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,79 @@
+=== Plugin Name ===
+Contributors: christopherross
+Plugin URI: http://thisismyurl.com/plugins/get-better-excerpt
+Tags: wordpress,easy,scheduled,posts
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5962435
+Requires at least: 2.0.0
+Tested up to: 2.7.1
+Stable tag: 0.0.1
+
+
+The Get Better Excerpt plugin works almost identical to the built in get_the_excerpt() and the_excerpt() functions except it returns whole words instead of cutting off the excerpt as the existing function does.
+
+== Description ==
+
+The Get Better Excerpt plugin works almost identical to the built in get_the_excerpt() and the_excerpt() functions except it returns whole words instead of cutting off the excerpt as the existing function does.
+
+== Installation ==
+
+To install the plugin, please upload the folder to your plugins folder and active the plugin.
+
+== Screenshots ==
+
+== Updates ==
+Updates to the plugin will be posted here, to [thisismyurl](http://www.thisismyurl.com/plugins/get-better-excerpt)
+
+== Frequently Asked Questions ==
+
+= How do I display the results? =
+
+Insert the following code into your WordPress theme files: 
+
+= General results =
+ithout passing any parameters, the plugin will return ten results or fewer depending on how many posts you have.
+
+ get_better_excerpt();
+
+
+= Altering the before and after values =
+By default the plugin wraps your code in list item (&lt;li&gt;) tags but you can specify how to format the results using the following code:
+
+ get_better_excerpt('before=&lt;p&gt;&amp;after=&lt;/p&gt;');
+
+= Adding a Link = 
+If you'd like to link to the post (remember it's not live yet) you can do so by calling:
+
+ get_better_excerpt('link=true'); 
+
+
+= How many words? = 
+You can specify the number of words returned using the option:
+
+ get_better_excerpt('words=20'); 
+ 
+ = Skip the excerpt? = 
+If you would like to load the content directly, skipping the entered excerpt:
+
+ get_better_excerpt('skipexcerpt=true'); 
+
+ = Include a trailing character? = 
+By default the plugin includes a ... after the excerpt, you can remove it or change it by altering:
+
+ get_better_excerpt('trail='); 
+
+= Echo vs. Return =
+Finally, if you'd like to copy the results into a variable you can return the results as follows:
+
+ get_better_excerpt('show=false'); 
+
+
+
+= Is this plugin stable? = 
+
+Technically yes. Until I upgrade the version to 1.x, I still consider it to be in development but yes it has been tested and works well.
+
+== Donations ==
+If you would like to donate to help support future development of this tool, please visit [thisismyurl](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5962435)
+
+
+== Change Log ==
